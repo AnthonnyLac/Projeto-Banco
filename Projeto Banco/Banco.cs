4d3276eb3b3e7ea4be86cbb0f84a4438c8cb4d07
AnthonnyLac <anthonny_leonardo1@hotmail.com>
2021-11-25 21:26:44 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Banco/Banco.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Banco
+{
+    class Banco
+    {
+        private string _nome;
+        private const int Taxa = 5;
+        public int NumeroConta { get; private set; }
+        public double Saldo {get; set;}
+
+        public Banco(string nome, int numeroConta) 
+        {
+            _nome = nome;
+            NumeroConta = numeroConta;
+        }
+        public Banco(string nome, int numeroConta, double saldo ) : this (nome, numeroConta)
+        {
+            Saldo = saldo;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        public bool escolha(string menuEscolha) 
+        {
+            if (menuEscolha == "s")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Deposito(double deposito) 
+        {
+            Saldo += deposito;
+        }
+        public void Saque(double saque)
+        {
+            Saldo -= saque;
+            Saldo -= Taxa;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados Atualizados!\nConta {NumeroConta}, Titular: {_nome}, Saldo: R${Saldo.ToString("F2")}";
+        }
+    }
+}
---
+++ Projeto Banco/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Projeto_Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposito = 0;
+            Console.WriteLine("Entre o numero da Conta:");
+            int numeroConta = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o nome do Titular da Conta:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Haverá deposito inicial (s/n)?");
+            string escolha = Console.ReadLine();
+            
+            if (escolha == "s")
+            {
+                Console.WriteLine("Entre valor para saldo inicial:");
+                deposito = Convert.ToDouble(Console.ReadLine());
+            }
+            var user1 = new Banco(nome, numeroConta, deposito);
+            Console.Clear();
+            Console.WriteLine(user1);
+            
+            Console.WriteLine("Entre um valor para deposito:");
+            user1.Deposito(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(user1);
+
+            Console.WriteLine("Insira um valor para saque:");
+            user1.Saque(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(user1);
+        }
+    }
+}
